7a139bd1c03c3941b4967568e9afa7d1915ea3ab
John Lyons <jwlyons2015@gmail.com>
2026-04-04 13:11:15 -0400

    Enabled running server from main root index.ts than inside of packages/client or packages/server

--- index.ts
+++ index.ts
@@ -1,1 +1,18 @@
-console.log("Hello via Bun!");
+import concurrently from 'concurrently';
+
+
+// instead of running file inside of packages/server, run it in main directory
+concurrently([
+    {
+        name: 'server',
+        command: 'bun run dev',
+        cwd: 'packages/server',
+        prefixColor: 'cyan'
+    },
+    {
+        name: 'client',
+        command: 'bun run dev',
+        cwd: 'packages/client',
+        prefixColor: 'green'
+    }
+])
