50267a2b1c0f5991c7e140be490253c79f006bb0
John Lyons <jwlyons2015@gmail.com>
2026-04-07 11:17:54 -0400

    Used Prettier to format my code

--- index.ts
+++ index.ts
@@ -1,18 +1,17 @@
 import concurrently from 'concurrently';
 
-
 // instead of running file inside of packages/server, run it in main directory
 concurrently([
-    {
-        name: 'server',
-        command: 'bun run dev',
-        cwd: 'packages/server',
-        prefixColor: 'cyan'
-    },
-    {
-        name: 'client',
-        command: 'bun run dev',
-        cwd: 'packages/client',
-        prefixColor: 'green'
-    }
-])
+   {
+      name: 'server',
+      command: 'bun run dev',
+      cwd: 'packages/server',
+      prefixColor: 'cyan',
+   },
+   {
+      name: 'client',
+      command: 'bun run dev',
+      cwd: 'packages/client',
+      prefixColor: 'green',
+   },
+]);
--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,28 +1,28 @@
-import { useState, useEffect } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "./assets/vite.svg";
-import heroImg from "./assets/hero.png";
-import "./App.css";
-import { Button } from "./components/ui/button";
+import { useState, useEffect } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from './assets/vite.svg';
+import heroImg from './assets/hero.png';
+import './App.css';
+import { Button } from './components/ui/button';
 
 function App() {
-  const [message, setMessage] = useState("");
+   const [message, setMessage] = useState('');
 
-  // use proxy to automatically forward all requests at /api
-  useEffect(() => {
-    fetch("/api/hello")
-      .then((res) => res.json())
-      .then((data) => setMessage(data.message));
-  }, []);
+   // use proxy to automatically forward all requests at /api
+   useEffect(() => {
+      fetch('/api/hello')
+         .then((res) => res.json())
+         .then((data) => setMessage(data.message));
+   }, []);
 
-  return (
-    <>
-      <div className="p-4">
-        <p className="font-bold text-3xl">{message}</p>
-        <Button>Click me</Button>
-      </div>
-    </>
-  );
+   return (
+      <>
+         <div className="p-4">
+            <p className="font-bold text-3xl">{message}</p>
+            <Button>Click me</Button>
+         </div>
+      </>
+   );
 }
 
 export default App;
